b75518d14519a91ad0187cdecbda4fc550f47407
kermilov <xharderx@yandex.ru>
2024-09-12 14:43:16 +0300

    удалил .sync, добавил тест кейсов и контроль символов к экранировке

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,29 +9,46 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+var allowToSafe = map[rune]struct{}{
+	'1':  {},
+	'2':  {},
+	'3':  {},
+	'4':  {},
+	'5':  {},
+	'6':  {},
+	'7':  {},
+	'8':  {},
+	'9':  {},
+	'\\': {},
+}
+
 func Unpack(in string) (string, error) {
 	var strBuilder strings.Builder
 	var prevRune rune
 	var prevIsCounter bool
 	var prevIsSafe bool
-	var runeCount int = utf8.RuneCountInString(in)
+	runeCount := utf8.RuneCountInString(in)
 	for i, currentRune := range in {
 		currentStr := string(currentRune)
 		prevStr := string(prevRune)
 		currentIsSafe := currentStr == `\` && (prevStr != `\` || !prevIsSafe)
 
 		repeatCount, err := strconv.Atoi(currentStr)
-		currentIsCounter := err == nil && !prevIsSafe		
-		
+		currentIsCounter := err == nil && !prevIsSafe
+
 		if i == 0 {
 			if currentIsCounter {
 				return "", ErrInvalidString
-			}			
+			}
 			prevRune = currentRune
 			continue
 		}
 
 		if prevIsCounter && currentIsCounter {
+			return "", ErrInvalidString
+		}
+
+		if _, currentIsAllowToSafe := allowToSafe[currentRune]; prevIsSafe && !currentIsAllowToSafe {
 			return "", ErrInvalidString
 		}
 
