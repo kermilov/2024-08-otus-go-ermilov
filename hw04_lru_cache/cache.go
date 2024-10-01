c8fcb883ae32a03a225941dc99f6c3a320b56184
kermilov <xharderx@yandex.ru>
2024-10-02 00:20:21 +0300

    hw04_lru_cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,45 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type lruCacheValue struct {
+	key   Key
+	value interface{}
+}
+
+func (l *lruCache) Clear() {
+	l.items = make(map[Key]*ListItem, l.capacity)
+	l.queue = NewList()
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	val, isExist := l.items[key]
+	if isExist {
+		l.queue.MoveToFront(val)
+		return val.Value.(lruCacheValue).value, true
+	}
+	return nil, false
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	existValue, isExist := l.items[key]
+	if isExist && existValue.Value == value {
+		l.queue.MoveToFront(existValue)
+		return true
+	} else if isExist {
+		l.queue.Remove(existValue)
+	}
+	l.items[key] = l.queue.PushFront(lruCacheValue{key, value})
+	if !isExist && l.queue.Len() > l.capacity {
+		lastInQueue := l.queue.Back()
+		delete(l.items, lastInQueue.Value.(lruCacheValue).key)
+		l.queue.Remove(lastInQueue)
+	}
+	return isExist
 }
 
 func NewCache(capacity int) Cache {
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,97 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	listItems map[*ListItem]struct{}
+	front     *ListItem
+	back      *ListItem
+}
+
+func (l *list) Len() int {
+	return len(l.listItems)
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	result := &ListItem{
+		Value: v,
+	}
+	l.listItems[result] = struct{}{}
+	front := l.front
+	if front != nil {
+		result.Next = front
+		front.Prev = result
+	}
+	l.front = result
+	if l.back == nil {
+		l.back = result
+	}
+	return result
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	result := &ListItem{
+		Value: v,
+	}
+	l.listItems[result] = struct{}{}
+	back := l.back
+	if back != nil {
+		result.Prev = back
+		back.Next = result
+	}
+	l.back = result
+	if l.front == nil {
+		l.front = result
+	}
+	return result
+}
+
+func (l *list) Remove(i *ListItem) {
+	next := i.Next
+	prev := i.Prev
+	if next != nil {
+		next.Prev = prev
+	}
+	if prev != nil {
+		prev.Next = next
+	}
+	if i == l.front {
+		l.front = next
+	}
+	if i == l.back {
+		l.back = prev
+	}
+	delete(l.listItems, i)
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.front {
+		return
+	}
+	next := i.Next
+	prev := i.Prev
+	if next != nil {
+		next.Prev = prev
+	}
+	if prev != nil {
+		prev.Next = next
+	}
+	if i == l.back {
+		l.back = prev
+	}
+	i.Next = l.front
+	i.Prev = nil
+	l.front = i
 }
 
 func NewList() List {
-	return new(list)
+	result := new(list)
+	result.listItems = make(map[*ListItem]struct{})
+	return result
 }
