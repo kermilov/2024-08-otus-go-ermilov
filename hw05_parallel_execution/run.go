140b246e368803c28e68214c10030256b3f4a8ca
kermilov <xharderx@yandex.ru>
2024-10-08 01:10:35 +0300

    Неправильно понял задание, всё переделал

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"sync"
 	"sync/atomic"
 )
 
@@ -12,31 +11,45 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	var countErrors int32
-	taskCount := len(tasks)
-	for start := 0; start < taskCount; start += n {
-		end := min(start+n, taskCount)
-		run(tasks[start:end], &countErrors, m)
-		if int(atomic.LoadInt32(&countErrors)) >= m {
-			return ErrErrorsLimitExceeded
-		}
-	}
-	return nil
+	taskChannel := publishTasks(tasks)
+	return <-runWorkers(n, m, taskChannel)
 }
 
-func run(tasks []Task, countErrors *int32, m int) {
-	wg := sync.WaitGroup{}
+func publishTasks(tasks []Task) chan Task {
+	taskChannel := make(chan Task, len(tasks))
 	for _, task := range tasks {
-		wg.Add(1)
-		go func(task Task) {
-			defer wg.Done()
-			if int(atomic.LoadInt32(countErrors)) >= m {
+		taskChannel <- task
+	}
+	close(taskChannel)
+	return taskChannel
+}
+
+func runWorkers(n, m int, taskChannel chan Task) chan error {
+	var countErrors, countAll int32
+	finishChannel := make(chan error, 1)
+	for i := 0; i < n; i++ {
+		go worker(taskChannel, finishChannel, &countErrors, &countAll, m)
+	}
+	return finishChannel
+}
+
+func worker(taskChannel chan Task, finishChannel chan error, countErrors, countAll *int32, m int) {
+	for task := range taskChannel {
+		cur := int(atomic.LoadInt32(countErrors))
+		if cur != 0 && cur >= m {
+			return
+		}
+		if err := task(); err != nil {
+			inc := int(atomic.AddInt32(countErrors, 1))
+			if inc >= m {
+				if inc == m || (inc == 1 && m == 0) {
+					finishChannel <- ErrErrorsLimitExceeded
+				}
 				return
 			}
-			if err := task(); err != nil {
-				atomic.AddInt32(countErrors, 1)
-			}
-		}(task)
+		}
+		if int(atomic.AddInt32(countAll, 1)) == cap(taskChannel) {
+			finishChannel <- nil
+		}
 	}
-	wg.Wait()
 }
