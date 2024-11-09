e7d2639380e86ca930e0877d8e079a270a1968ee
kermilov <xharderx@yandex.ru>
2024-11-09 23:44:49 +0300

    hw07_file_copying, первая часть дз

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,38 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer fileFrom.Close()
+	fileFromStat, err := fileFrom.Stat()
+	if err != nil {
+		return err
+	}
+	if offset > 0 && fileFromStat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit == 0 {
+		limit = fileFromStat.Size() - offset
+	}
+	copyContent := make([]byte, limit+1)
+	n, err := fileFrom.ReadAt(copyContent, offset)
+	if err != nil {
+		if err == io.EOF {
+			copyContent = copyContent[:n]
+		} else {
+			return err
+		}
+	}
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fileTo.Close()
+	_, err = fileTo.Write(copyContent)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,36 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const toPath = "testdata/out.txt"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		offset           int64
+		limit            int64
+		expectedFilePath string
+	}{
+		{0, 0, "testdata/out_offset0_limit0.txt"},
+		{0, 10, "testdata/out_offset0_limit10.txt"},
+		{0, 1000, "testdata/out_offset0_limit1000.txt"},
+		{0, 10000, "testdata/out_offset0_limit10000.txt"},
+		{100, 1000, "testdata/out_offset100_limit1000.txt"},
+		{6000, 1000, "testdata/out_offset6000_limit1000.txt"},
+	}
+	for _, tc := range tests {
+		err := Copy("testdata/input.txt", toPath, tc.offset, tc.limit)
+		require.Nil(t, err)
+		actualContent, err := os.ReadFile(toPath)
+		require.Nil(t, err)
+		expectedContent, err := os.ReadFile(tc.expectedFilePath)
+		require.Nil(t, err)
+		require.Equal(t, expectedContent, actualContent)
+		err = os.Remove(toPath)
+		require.Nil(t, err)
+	}
 }
