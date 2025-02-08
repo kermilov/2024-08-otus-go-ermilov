a887253aadc5b6a0ebaa06f8243228f3ab71791f
kermilov <xharderx@yandex.ru>
2025-02-08 21:21:45 +0300

    hw05_parallel_execution Причесал по косметике чисто для себя

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -12,11 +12,23 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	x := min(n, len(tasks))
-	taskChannel := make(chan Task, x)
-	var wg sync.WaitGroup
+	taskCount := len(tasks)
+	workerCount := min(n, taskCount)
+	taskChannel := make(chan Task, workerCount)
 	var countErrors atomic.Int64
-	for i := 0; i < x; i++ {
+	// публикация заданий
+	go func() {
+		for i := 0; i < taskCount; i++ {
+			if isCountErrExeed(&countErrors, m) {
+				break
+			}
+			taskChannel <- tasks[i]
+		}
+		close(taskChannel)
+	}()
+	// запуск обработчиков заданий
+	var wg sync.WaitGroup
+	for i := 0; i < workerCount; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
@@ -31,14 +43,6 @@ func Run(tasks []Task, n, m int) error {
 			}
 		}()
 	}
-	taskCount := len(tasks)
-	for i := 0; i < taskCount; i++ {
-		if isCountErrExeed(&countErrors, m) {
-			break
-		}
-		taskChannel <- tasks[i]
-	}
-	close(taskChannel)
 	wg.Wait()
 	if isCountErrExeed(&countErrors, m) {
 		return ErrErrorsLimitExceeded
